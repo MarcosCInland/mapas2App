10ff57267df1348f2d81141bbdf677bab602a217
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-10 16:13:19 -0600

    display map

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Map } from 'mapbox-gl';
 import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styles: [
-  ]
+  styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent implements OnInit {
+export class MapViewComponent implements AfterViewInit {
 
+  @ViewChild('mapDiv') mapDivElement!: ElementRef
+  
   constructor(private placesService: PlacesService) { }
-
-  ngOnInit(): void {
-    console.log(this.placesService.userLocation);
+  
+  ngAfterViewInit(): void {
+    const map = new Map({
+      container: this.mapDivElement.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.placesService.userLocation, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      });
+    //
+    projection: 'globe' // display the map as a 3D globe
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl';
+Mapboxgl.accessToken = 'pk.eyJ1IjoibWFyY29zYzEwIiwiYSI6ImNsN2YwMXR1czA1aDMzcG80NDM1eGhrMnMifQ.hFhEr3PIPvWO5nJQrPFbfA';
+
 if (!navigator.geolocation) {
   alert('Navegador no soporta la geolocation');
   throw new Error('Navegador no soporta la geolocation');
